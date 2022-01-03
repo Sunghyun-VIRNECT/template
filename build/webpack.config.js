8e6a64476a854348fa2a488b6acd1770d025aa78
jane.sim <88020285+seyoung-VIRNECT@users.noreply.github.com>
2022-01-03 16:51:23 +0900

    update: webpack config 변경

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,14 +1,34 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
+const { Base64 } = require('js-base64')
+
+const extractCSS = new MiniCssExtractPlugin({
+  filename: '[name].css',
+})
 
 module.exports = {
   resolve: {
     extensions: ['.js', '.vue'],
     alias: {
       '~': path.resolve(__dirname, '../src'),
+      virnect: path.resolve(__dirname, '../node_modules/@virnect'),
       assets: path.resolve(__dirname, '../src/assets'),
+      root: path.resolve(__dirname, '../'),
+      api: path.join(__dirname, '../src/api'),
+      apps: path.join(__dirname, '../src/apps'),
+      components: path.join(__dirname, '../src/components'),
+      mixins: path.join(__dirname, '../src/mixins'),
+      languages: path.join(__dirname, '../src/languages'),
+      // lib: path.join(__dirname, '../src/lib'),
+      configs: path.join(__dirname, '../src/configs'),
+      layouts: path.join(__dirname, '../src/layouts'),
+      routers: path.join(__dirname, '../src/routers'),
+      stores: path.join(__dirname, '../src/stores'),
+      // element: path.join(__dirname, '../theme/'),
+      tests: path.join(__dirname, '../tests/'),
     },
   },
 
@@ -30,9 +50,23 @@ module.exports = {
         use: [
           'vue-style-loader',
           'style-loader',
-          'css-loader',
+          { loader: 'css-loader', options: { sourceMap: true } },
           'postcss-loader',
-          'sass-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-resources-loader',
+            options: {
+              resources: path.resolve(
+                __dirname,
+                '../src/assets/scss/mixin.scss',
+              ),
+            },
+          },
         ],
       },
       {
@@ -41,8 +75,55 @@ module.exports = {
         use: ['babel-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif|webp)$/,
-        use: 'file-loader',
+        test: /\.(png|jpg|jpeg|gif|eot|otf|ttf|woff|woff2|svg|svgz|ico|pdf|rss|xml|txt)(\?.+)?$/,
+        exclude: /(favicon\.png|VIRNECT_ROI\.svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'assets/[name].[hash:7].[ext]',
+            },
+          },
+        ],
+      },
+      // {
+      //   test: /\.html$/,
+      //   use: [
+      //     {
+      //       loader: 'html-loader',
+      //       options: {
+      //         root: path.resolve(__dirname, 'src'),
+      //         attrs: ['img:src', 'link:href'],
+      //       },
+      //     },
+      //   ],
+      // },
+      // pdfkit
+      {
+        test: /VIRNECT_ROI\.svg$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: 'assets/[name].[hash:7].[ext]',
+              generator: (content, mimetype) => {
+                return `data:${mimetype},${Base64.encode(content)}`
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(mp4|ogg|mp3|pdf|md)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'assets/[name].[hash:7].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
@@ -55,6 +136,7 @@ module.exports = {
       patterns: [{ from: 'static' }],
     }),
     new VueLoaderPlugin(),
+    extractCSS,
   ],
 
   devServer: {
