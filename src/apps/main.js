e7d2e0abf1f64b6edd579c1387efa42ccf998cf5
jane.sim <88020285+seyoung-VIRNECT@users.noreply.github.com>
2022-01-03 16:52:31 +0900

    add: virnect 공통 컴포넌트 설치

--- src/apps/main.js
+++ src/apps/main.js
@@ -3,6 +3,9 @@ import App from '~/App'
 import store from '~/stores'
 import router from '~/routers'
 import i18n from '~/languages'
+import '@virnect/ui-assets/css/base.old.css'
+import * as Virnect from '@virnect/components'
+import '../assets/scss/_components.scss'
 
 const app = createApp(App)
 
@@ -10,3 +13,8 @@ app.use(store).use(router).use(i18n).mount('#app')
 
 app.config.globalProperties.$store = store
 app.config.globalProperties.$router = router
+
+// add VirnectComponents
+Object.entries(Virnect).map(([componentName, component]) => {
+  app.component(`Virnect${componentName}`, component)
+})
